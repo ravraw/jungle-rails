3918b8348b7ad1daa3b2bb180c614ace85af81ca
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-06 22:34:11 -0700

    user can login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def index
+  
+    redirect_to action: 'new'
+  end
+
+  def new
+
+  end
+
+  def create
+   user = User.find_by_email(params[:email])
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      # flash[:notice] = "Email or password is invalid"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+ 
   def index
+      redirect_to action: 'new'
   end
 
   def new
+     @user = User.new()
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      render action: 'new'
+    end
   end
+
+ private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
