54b4e0211a2537dc4dc0eaf178ec53ff365272a2
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-06 20:37:16 -0700

    added user model, session and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create] 
     end
 
+  resources :users, only: [:index, :new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
