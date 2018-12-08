ff4226c9fb3cfbc2b8d43a565da764c016278e9c
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-07 18:12:03 -0700

    added admin authentican to admin controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
   def create_order(stripe_charge)
     order = Order.new(
       # email: params[:stripeEmail],
-      email: session[:email],
+      email:session[:email],
       total_cents: cart_subtotal_cents,
       stripe_charge_id: stripe_charge.id, # returned by stripe
     )
