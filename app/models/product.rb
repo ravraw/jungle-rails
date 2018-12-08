f6bb6cc6a3e3bceec7d32a3c63b965d06bf17f3f
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-08 15:32:08 -0700

    added validation for user_id,product_id, description and rating

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,5 +10,4 @@ class Product < ActiveRecord::Base
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
-
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
 
   belongs_to :product
+  validates description presence: true
+  validates rating, presence: true
+  validates user_id, presence: true
+  validates product_id, presence: true
 end
